8050825060755837dd460cfa3d4c1d9e0207a0ff
yuki <188823252+yukihogehoge@users.noreply.github.com>
2024-12-23 23:24:03 +0900

    フレームを取得できているかをチェックするコードを追加

--- Shooting_camera.py
+++ Shooting_camera.py
@@ -3,35 +3,36 @@ from time import sleep
 from datetime import datetime
 
 # カメラの初期化
-cap = cv2.VideoCapture(0)
+for i in range(1, 32):
+  cap = cv2.VideoCapture(i)
 
-if not cap.isOpened():
-  print("正常に読み込めませんでした。")
-  exit()
+  if not cap.isOpened():
+    print("正常に読み込めませんでした。")
+    exit()
 
-try:
-  while True:
-    # frameをキャプチャ
-    ret, frame = cap.read()
-    if not ret:
-      print("フレームが取得できませんでした。")
-      sleep(5)
-      continue
+  try:
+    while True:
+      # frameをキャプチャ
+      ret, frame = cap.read()
+      if not ret:
+        print("フレームが取得できませんでした。")
+        sleep(5)
+        break
 
-    # 現在時刻でファイル名を生成
-    timestamp = datetime.now().strftime("%Y年%m月%d日%H時%M分%S秒")
-    filename = f"image_{timestamp}.jpg"
+      # 現在時刻でファイル名を生成
+      timestamp = datetime.now().strftime("%Y年%m月%d日%H時%M分%S秒")
+      filename = f"image_{timestamp}.jpg"
 
-    # 写真を保存
-    print(f"撮影中:{filename}")
-    cv2.imwrite(filename, frame)
+      # 写真を保存
+      print(f"撮影中:{filename}")
+      cv2.imwrite(filename, frame)
 
-    # 15分間待機
-    sleep(60 * 15)
+      # 15分間待機
+      sleep(60 * 15)
 
-except KeyboardInterrupt:
-  print("キーボード入力が行われたので終了します。")
+  except KeyboardInterrupt:
+    print("キーボード入力が行われたので終了します。")
 
-finally:
-  cap.release()
-  cv2.destroyAllWindows()
+  finally:
+    cap.release()
+    cv2.destroyAllWindows()
