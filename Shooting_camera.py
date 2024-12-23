0eac759c67f0c299a2a6b81d4e523d7b0c6c3a3d
yuki <188823252+yukihogehoge@users.noreply.github.com>
2024-12-23 23:44:21 +0900

    デバイスIDを0からに変更

--- Shooting_camera.py
+++ Shooting_camera.py
@@ -3,7 +3,7 @@ from time import sleep
 from datetime import datetime
 
 # カメラの初期化
-for i in range(1, 32):
+for i in range(0, 32):
   cap = cv2.VideoCapture(i, cv2.CAP_V4L2)
 
   if not cap.isOpened():
