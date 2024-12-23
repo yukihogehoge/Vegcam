0c3528d9467a0217123fa280db7a8cc6f1ad4db6
yuki <188823252+yukihogehoge@users.noreply.github.com>
2024-12-23 21:04:56 +0900

    base code create

---
+++ Shooting_camera.py
@@ -0,0 +1,33 @@
+import cv2
+from time import sleep
+from datetime import datetime
+
+# カメラの初期化
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+  print("正常に読み込めませんでした。")
+  exit()
+
+try:
+  while True:
+    # frameをキャプチャ
+    ret, frame = cap.read()
+
+    # 現在時刻でファイル名を生成
+    timestamp = datetime.now().strftime("%Y年%m月%d日%H時%M分%S秒")
+    filename = f"image_{timestamp}.jpg"
+
+    # 写真を保存
+    print(f"撮影中:{filename}")
+    cv2.imwrite(filename, frame)
+
+    # 15分間待機
+    sleep(60 * 15)
+
+except KeyboardInterrupt:
+  print("キーボード入力が行われたので終了します。")
+
+finally:
+  cap.release()
+  cv2.destroyAllWindows()
