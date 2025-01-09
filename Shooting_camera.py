7dd6e37e096be312cb070b3e795589c6d553f294
yuki <188823252+yukihogehoge@users.noreply.github.com>
2025-01-09 16:08:41 +0900

    各行にコメントを追加

--- Shooting_camera.py
+++ Shooting_camera.py
@@ -1,10 +1,19 @@
-from picamera2 import Picamera2, Preview
-from libcamera import controls
+# カメラ制御のためのクラスであるpicamera2をインポート
+from picamera2 import Picamera2
+# controlsはカメラ設定の調整時に使用（使用していない）
+# from libcamera import controls
+# 一定時間停止するためにPython標準ライブラリのtimeモジュールからsleep関数をインポート
 from time import sleep
+"""
+日付や時刻を扱うためのdatetimeモジュールをインポート
+ファイル名作成時に使用
+"""
 from datetime import datetime
 
 # Picamera2の初期化
+# Picamera2のインスタンスを作成
 picam2 = Picamera2()
+# 高品質な写真を撮るための設定を生成
 picam2.configure(picam2.create_still_configuration())
 
 # カメラを起動
@@ -18,6 +27,7 @@ try:
 
         # 写真を撮影して保存
         print(f"撮影中: {filename}")
+        # 現在の映像を静止画として保存
         picam2.capture_file(filename)
 
         # 15分間待機
