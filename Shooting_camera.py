662114e052cc2a103c7dc7ad3ab849c9cfbcd258
yuki <188823252+yukihogehoge@users.noreply.github.com>
2025-01-16 22:24:39 +0900

    インデントを変更

--- Shooting_camera.py
+++ Shooting_camera.py
@@ -11,32 +11,32 @@ from time import sleep
 from datetime import datetime
 
 def capture_images():
-    # Picamera2の初期化
-    # Picamera2のインスタンスを作成
-    picam2 = Picamera2()
-    # 高品質な写真を撮るための設定を生成
-    picam2.configure(picam2.create_still_configuration())
+  # Picamera2の初期化
+  # Picamera2のインスタンスを作成
+  picam2 = Picamera2()
+  # 高品質な写真を撮るための設定を生成
+  picam2.configure(picam2.create_still_configuration())
 
-    # カメラを起動
-    picam2.start()
+  # カメラを起動
+  picam2.start()
 
-    try:
-        while True:
-            # 現在時刻でファイル名を生成
-            timestamp = datetime.now().strftime("%Y年%m月%d日%H時%M分%S秒")
-            filename = f"image_{timestamp}.jpg"
+  try:
+    while True:
+      # 現在時刻でファイル名を生成
+      timestamp = datetime.now().strftime("%Y年%m月%d日%H時%M分%S秒")
+      filename = f"image_{timestamp}.jpg"
 
-            # 写真を撮影して保存
-            print(f"撮影中: {filename}")
-            # 現在の映像を静止画として保存
-            picam2.capture_file(filename)
+      # 写真を撮影して保存
+      print(f"撮影中: {filename}")
+      # 現在の映像を静止画として保存
+      picam2.capture_file(filename)
 
-            # 15分間待機
-            sleep(60 * 15)
+      # 15分間待機
+      sleep(60 * 15)
 
-    except KeyboardInterrupt:
-        print("キーボード入力が行われたので終了します。")
+  except KeyboardInterrupt:
+    print("キーボード入力が行われたので終了します。")
 
-    finally:
-        # カメラを停止
-        picam2.stop()
+  finally:
+    # カメラを停止
+    picam2.stop()
