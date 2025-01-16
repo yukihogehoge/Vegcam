51a4af1362ec0592c5c2b2f04a032aa5e7450000
yuki <188823252+yukihogehoge@users.noreply.github.com>
2025-01-17 05:28:47 +0900

    driveの保存先を特定のフォルダに指定

--- Google_drive_upload.py
+++ Google_drive_upload.py
@@ -8,9 +8,15 @@ def upload_to_google_drive(file_path):
     gauth.LocalWebserverAuth()  # ブラウザを使用して認証
     drive = GoogleDrive(gauth)
 
+    # アップロード先のフォルダID
+    folder_id = "1is749wEyc_7X-9s-oUx5cbJvjQguK9Yv"
+
     # ファイルをアップロード
     file_name = file_path.split('/')[-1]
-    file = drive.CreateFile({'title': file_name})  # Google Drive上のファイル名
+    file = drive.CreateFile({
+        'title': file_name,       # Google Drive上のファイル名
+        'parents': [{'id': folder_id}]  # アップロード先フォルダを指定
+    })
     file.SetContentFile(file_path)
     file.Upload()
 
