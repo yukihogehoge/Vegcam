7a6eb0a9255254f0b4fb11b661f964f042511a40
yuki <188823252+yukihogehoge@users.noreply.github.com>
2025-02-05 19:44:39 +0900

    検出部分を修正。Picamera2 を使って撮影した画像を cv2_img に変換し、YOLOモデルで処理するように変更。

--- Detect.py
+++ Detect.py
@@ -9,17 +9,20 @@ def detect__objects_in_image(image_path):
 
   # 画像の読み込み
   if image_path is not None:
-    results = model(image_path)
     
-    bytes_data = image_path.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8),cv2.IMREAD_COLOR)
-    results = model(cv2_img,conf=0.5,classes=[0])
-    output_img = results[0].plot(labels=True,conf=True)
+    cv2_img = cv2.imread(image_path)
+    
+    # 物体検出
+    results = model(cv2_img, conf=0.5, classes=[0])
+    
+    # 結果を描画
+    output_img = results[0].plot(labels=True, conf=True)
     output_img = cv2.cvtColor(output_img, cv2.COLOR_BGR2RGB)
-
+    
+    # 人の数をカウント
     categories = results[0].boxes.cls
     person_num = len(categories)
-
-    #output
-    st.image(output_img, caption = '出力画像')
-    st.text(f'人数は{person_num}人です。')
+    
+    # 出力
+    st.image(output_img, caption='出力画像')
+    st.text(f'人数は {person_num} 人です。')
